080cbeea055979c9e34290a5b3a49789dcfaf72e
yuryvn <yury.v.n@gmail.com>
2016-08-21 15:07:21 +0200

    first sync of cpp

---
+++ stage2/stage2/stage2.cpp
@@ -0,0 +1,14 @@
+// stage2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
